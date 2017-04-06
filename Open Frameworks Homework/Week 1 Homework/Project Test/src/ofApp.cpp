42e9c5e6b06bb6bb5dcdde105c1a1dd09b5799ea
Jacob Fisher <fishj900@newschool.edu>
2017-04-05 23:29:01 -0400

    Add files via upload

---
+++ Open Frameworks Homework/Week 1 Homework/Project Test/src/ofApp.cpp
@@ -0,0 +1,48 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    pug.load("pug.jpg");
+    Conversations.load("Conversations.wav");
+    Conversations.play();
+    
+    Bubbles.load("Bubbles.mp4");
+    Bubbles.play();
+    ofBackground(500,440,200);
+    
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+Bubbles.update();
+    for (auto &vert : line.getVertices()){
+        vert.x += ofRandom(-1,1);
+        vert.y += ofRandom(-1,1);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    pug.draw(0,0,800,800);
+    ofSetColor(1000,900,500);
+    
+    ofDrawTriangle(400, 400, 400, 400, 400, 400);
+    
+    Bubbles.draw(300,200,400,300);
+    
+    line.draw();
+    ofDrawBitmapString("Puppies and Bubbles. Puppies and Bubbles. Puppies and Bubbles. Puppies and Bubbles. Puppies and Bubbles. Puppies and Bubbles.", 100, 400);
+    
+    
+}
+
+void ofApp::mouseDragged(int x, int y, int button){
+    ofPoint pt;
+    pt.set(x,y);
+    line.addVertex(pt);
+}
+void ofApp::mousePressed(int x, int y, int button){
+    line.clear();
+
+}
---
+++ Open Frameworks Homework/Week 1 Homework/Project Test/src/ofApp.h
@@ -0,0 +1,28 @@
+ #pragma once
+
+#include "ofMain.h"
+
+
+class ofApp : public ofBaseApp{
+    
+    public:
+		void setup();
+		void update();
+		void draw();
+    
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    
+    ofImage pug;
+    
+		ofSoundPlayer Conversations;
+    
+    ofVideoPlayer Bubbles;
+    
+    ofPolyline line;
+    
+    ofTrueTypeFont myfont;
+
+};
+
+
